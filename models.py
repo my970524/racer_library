462c32a71edbf6b9ad3ccaef17d1a45021bc77c2
[2기-2반] 김민지 <my970524@gmail.com>
2021-08-24 01:04:56 +0900

    gitignore 파일 test

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from db_connect import db
+from datetime import datetime
 
 class User(db.Model):
     __tablename__ = "user"
@@ -30,7 +31,7 @@ class Book(db.Model):
     description = db.Column(db.String(500), nullable=True)
     rating = db.Column(db.Integer, nullable=True)
         
-    def __init__(self, title, author, publisher, release_date, pages, isbn_code, quantity, introduction, description):
+    def __init__(self, title, author, publisher, release_date, pages, isbn_code, quantity, introduction, description, rating):
         self.title = title
         self.author = author
         self.publisher = publisher
@@ -45,9 +46,26 @@ class Book(db.Model):
 class User_Book(db.Model):
     __tablename__ = "user_book"
     id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
-    user_email = db.Column(db.String(320), db.ForeignKey('user.email'))
-    book_id = db.Column(db.Integer, db.ForeignKey('book.id'))
+    user_email = db.Column(db.String(320), db.ForeignKey('user.email'), nullable=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=True)
+    book_title = db.Column(db.String(100), nullable=True)
 
-    def __init__(self, user_email, book_id):
+    def __init__(self, user_email, book_id, book_title):
         self.user_email = user_email
         self.book_id = book_id
+        self.book_title = book_title
+
+class Review(db.Model):
+    __tablename__ = "review"
+    id = db.Column(db.Integer, primary_key=True, nullable=False, autoincrement=True)
+    user_email = db.Column(db.String(320), db.ForeignKey('user.email'), nullable=True)
+    book_id = db.Column(db.Integer, db.ForeignKey('book.id'), nullable=True)
+    rating = db.Column(db.Integer, nullable=True)
+    content = db.Column(db.Text())
+    posted_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __init__(self, user_email, book_id, rating, content):
+        self.user_email = user_email
+        self.book_id = book_id
+        self.rating = rating
+        self.content = content
--- views/api.py
+++ views/api.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, redirect, request, jsonify, Blueprint, session
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import and_
 from models import *
 from flask_bcrypt import Bcrypt
 
@@ -14,9 +15,12 @@ bcrypt = Bcrypt()
 @bp.route('/', methods=["GET"])
 def home():
     books = Book.query.all()
+    
+    if 'login' not in session:
+        return render_template('main.html', books=books)
 
     if session['login'] is not None:
-        user = User.query.filter(User.email == session['login']).first()
+        user = db.session.query(User).filter(User.email == session['login']).first()
         return render_template('main.html', books=books, user=user)
     else:
         return render_template('main.html', books=books)
@@ -34,7 +38,7 @@ def register():
         pw_hash = bcrypt.generate_password_hash(password)
 
         # 이미 등록된 사용자인지 검사
-        user = User.query.filter(User.email == email).first()
+        user = db.session.query(User).filter(User.email == email).first()
         if user is not None:
             return jsonify({"result": "email_check"})
         # 비밀번호 자릿수 검사
@@ -57,7 +61,7 @@ def login():
         email = request.form['email']
         password = request.form['pwd']
 
-        user = User.query.filter(User.email == email).first()
+        user = db.session.query(User).filter(User.email == email).first()
 
         if user is not None:
             if bcrypt.check_password_hash(user.password, password):
@@ -80,6 +84,8 @@ def borrow():
     book_id = request.form['book_id']
 
     book = db.session.query(Book).filter(Book.id == book_id).first()
+
+    book_title = book.title
     
     if 'login' not in session:
         return jsonify({"result": "login_fail"})
@@ -89,26 +95,76 @@ def borrow():
         else:
             if book.quantity > 0:
                 book.quantity -= 1 
-                user_book = User_Book(session['login'], book_id)
+                user_book = User_Book(session['login'], book_id, book_title)
                 db.session.add(user_book)
                 db.session.commit()
                 return jsonify({"result": "success"})
             else: 
                 return jsonify({"result": "fail"})
+
+
+# 대여기록
+@bp.route('/borrowed_books', methods=["GET"])
+def show_borrowed_books():
+    user = db.session.query(User).filter(User.email == session['login']).first()
+
+    borrowed_books = db.session.query(User_Book).filter(User_Book.user_email == user.email).all()
     
+    return render_template('borrowed_books.html', borrowed_books=borrowed_books, user=user)
+
+# 반납하기
+@bp.route('/return_book', methods=["PATCH"])
+def return_book():
+    rent_id = request.form['id']
+    borrowed_book = db.session.query(User_Book).filter(User_Book.id == rent_id).first()
+    book = db.session.query(Book).filter(Book.id == borrowed_book.book_id).first()
+    book.quantity += 1
+    db.session.delete(borrowed_book)
+    db.session.commit()
+
+    return jsonify({"result": "success"})
 
 # 책 상세 페이지
 @bp.route('/book/<int:book_id>', methods=["GET"])
 def book_info(book_id):
-    book = Book.query.filter(Book.id == book_id).first()
+    book = db.session.query(Book).filter(Book.id == book_id).first()
+    
+    reviews = db.session.query(Review).filter(Review.book_id == book_id).all()
 
     if 'login' not in session:
-        return render_template('book.html', book=book)
-    else:
-        if session['login'] is None:
-            return render_template('book.html', book=book)
-        else:
-            user = User.query.filter(User.email == session['login']).first()
-            return render_template('book.html', book=book, user=user)
+        return render_template('book.html', book=book, reviews=reviews)
 
-    return render_template('book.html', book=book)
+    if session['login'] is None:
+        return render_template('book.html', book=book, reviews=reviews)
+
+    user = db.session.query(User).filter(User.email == session['login']).first()
+    return render_template('book.html', book=book, reviews=reviews, user=user)
+
+# 리뷰 작성 페이지
+@bp.route('/review/<int:book_id>', methods=["POST"])
+def post_review(book_id):
+    rating = int(request.form['rating'])
+    content = request.form['content']
+    user_email = session['login']
+    book_id = book_id
+    
+    review = Review(user_email, book_id, rating, content)
+    db.session.add(review)
+    db.session.commit()
+
+    return jsonify({"result": 'success'})
+
+# 리뷰 삭제
+@bp.route('/delete_review/<int:book_id>/<int:review_id>', methods=["DELETE"])
+def delete_review(book_id, review_id):
+    review = db.session.query(Review).filter(Review.id == review_id).first()
+    book = db.session.query(Book).filter(Book.id == book_id).first()
+
+    if review.user_email != session['login']:
+        return jsonify({"result": "user_error"})
+    elif review.book_id != book.id:
+        return jsonify({"reuslt": "fail"})
+    else:
+        db.session.delete(review)
+        db.session.commit()
+        return jsonify({"result": "success"})
